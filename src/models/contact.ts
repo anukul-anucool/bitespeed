93efea0cb375fa9c94a34f142fb00a8dda9f2f4f
Anukul kumar <44088496+anukul-anucool@users.noreply.github.com>
2023-07-24 09:19:03 +0530

    Final changes

---
+++ src/models/contact.ts
@@ -0,0 +1,62 @@
+import { DataTypes, Model, ModelAttributes } from "sequelize";
+import { sequelize } from "../app"; // Assuming the Sequelize instance is created and exported in app.ts
+
+interface ContactAttributes {
+  id?: number;
+  phoneNumber?: string;
+  email?: string;
+  linkedId: number | null;
+  linkPrecedence: "primary" | "secondary";
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt?: Date | null;
+}
+
+class Contact extends Model<ContactAttributes> implements ContactAttributes {
+  public id?: number;
+  public phoneNumber?: string;
+  public email?: string;
+  public linkedId!: number | null;
+  public linkPrecedence!: "primary" | "secondary";
+  public createdAt!: Date;
+  public updatedAt!: Date;
+  public deletedAt?: Date | null;
+}
+
+const contactAttributes: ModelAttributes<Contact, ContactAttributes> = {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  phoneNumber: {
+    type: DataTypes.STRING,
+  },
+  email: {
+    type: DataTypes.STRING,
+  },
+  linkedId: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  linkPrecedence: {
+    type: DataTypes.ENUM("primary", "secondary"),
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+  },
+  deletedAt: {
+    type: DataTypes.DATE,
+    allowNull: true,
+  },
+};
+
+Contact.init(contactAttributes, {
+  sequelize, // Use the Sequelize instance here
+  tableName: "Contact",
+});
+
+export default Contact;
